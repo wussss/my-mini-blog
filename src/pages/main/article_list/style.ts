a16a0d3ae23f7723f821720dd64843eab38b798c
silan.wu <silan.wu@liulishuo.com>
2021-06-09 22:01:51 +0800

    refactor

--- src/pages/main/article_list/style.ts
+++ src/pages/main/article_list/style.ts
@@ -1,14 +1,12 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  width: 960px;
-  height: 800px;
+  width: 800px;
   margin: 70px auto;
   background: #fff;
+  position: relative;
+  border-radius: 4px;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.1);
   list-style: none;
   .navList {
     display: flex;
